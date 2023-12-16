c486c991e37443d34d95bfd49331fac66cf1b8d4
yun <dbstjdgur78@naver.com>
2023-12-17 03:31:45 +0900

    lms commit

---
+++ src/main/java/com/zerobase/fastlms/admin/model/CategoryInput.java
@@ -0,0 +1,13 @@
+package com.zerobase.fastlms.admin.model;
+
+
+import lombok.Data;
+
+@Data
+public class   CategoryInput {
+    long id;
+    String categoryName;
+    int sortValue;
+    boolean usingYn;
+    
+}
